099ab50e4878719c8769809f576ab20081289d62
zapfire88 <43233970+zapfire88@users.noreply.github.com>
2019-02-21 00:50:19 +0100

    Update Adventure_crawl.hs

--- Adventure_crawl.hs
+++ Adventure_crawl.hs
@@ -3,11 +3,30 @@
 data Action = Inspect | Take | Use | Move | Hit | Speak | Open
 
 type Map = [Room]
-type Room = [Name, Description, Item, PointOfInterest, PossibleDirections]
-type Name = String
+type Room = [Number, PossibleDirections, Description, Item, PointOfInterest]
+type Number = Int
 type Description = String
 type Item = String
 type PointOfInterest = String
-type PossibleDirections = [String]
+type PossibleDirections = [(Number, Number)]
 
+-- Edge list av alla rum --
+[(1, 2),
+(2, 1),(2, 3),(2, 7),
+(3, 2),(3, 4),
+(4, 3),(4, 5),(4, 6),
+(5, 4),
+(6, 4),(6, 13),
+(7, 2),(7, 8),
+(8, 7),(8, 9),(8, 11),
+(9, 8),(9, 10),
+(10, 9),
+(11, 8),(11, 12),
+(12, 11),
+(13, 6),(13, 14),(13, 15),
+(14, 13),
+(15, 13),(15, 16),
+(16, 15),(16, 17),
+(17, 16),(17, 18),
+(18, 17)]
 
