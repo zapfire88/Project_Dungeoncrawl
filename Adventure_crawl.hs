bd7f21e479c9444ebf615bdee1c07dde3fd2b5e6
zapfire88 <43233970+zapfire88@users.noreply.github.com>
2019-02-20 22:19:05 +0100

    Update Adventure_crawl.hs

--- Adventure_crawl.hs
+++ Adventure_crawl.hs
@@ -1,7 +1,12 @@
 
+
+data Action = Inspect | Take | Use | Move | Hit | Speak | Open
+
+type Map = [Room]
+type Room = [Description, Item, PointOfInterest]
 type Description = String
 type Item = String
 type PointOfInterest = String
-type Room = [Description, Item, PointOfInterest]
-type Map = [Room]
+
+
 
