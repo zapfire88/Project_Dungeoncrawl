8f1e19e980fdda642d05f57876a742e2aa291f48
zapfire88 <43233970+zapfire88@users.noreply.github.com>
2019-02-22 05:20:42 +0100

    Update Adventure_crawl.hs

--- Adventure_crawl.hs
+++ Adventure_crawl.hs
@@ -1,17 +1,19 @@
 -- Bara några exempel --
 
-data Action = Inspect | Take | Use | Move | Hit | Speak | Open
+data Action = Inspect | Take | Use | Move | Hit | Open
 
 type Bag = [Item]
 
 type Map = [Room]
-type Room = [Number, PossibleDirections, Description, PointOfInterest]
-type Number = Int
+type Room = [RoomNumber, PossibleDirections, Description, PointOfInterest]
+type RoomNumber = Int
 type Description = String
 type Item = String
 type PointOfInterest = [(Item, Bool)]
 type PossibleDirections = [Door]
 type Door = [Int, Bool, Bool] -- Int is the index (of doors in room). First Bool is for whether it is open, second if it is locked.
+type Chest = [Bool, Bool]
+
 
 
 -- Edge list av alla rum --
@@ -33,4 +35,17 @@ type Door = [Int, Bool, Bool] -- Int is the index (of doors in room). First Bool
 (16, 15),(16, 17),
 (17, 16),(17, 18),
 (18, 17)]
+
+start (
+
+runIntro = putStr ( unlines list)
+  where list = ["Finally, after many years of searching, you are now standing in front of it.", "To think that there was something, either some sort of a force field, or even stranger, a magic field that could hide it.","I will definitely discover something amazing here!"] 
+
+startGame :: IO ()
+startGame = do
+  runIntro
+  runGame start
+
+
+
 
