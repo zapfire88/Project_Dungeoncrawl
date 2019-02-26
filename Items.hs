47fe78496cc1456888910c26bc6f5fb72fff47fa
jagtarenpaj <43341239+jagtarenpaj@users.noreply.github.com>
2019-02-26 15:23:52 +0100

    Update Items.hs

--- Items.hs
+++ Items.hs
@@ -1,9 +1,9 @@
-
-
 -- Items --
 module Items(itemsList)
 where
 
 --itemList :: [(RoomNumber,[Item])]
-itemsList = [(1,[]), (2,[]),(3,[("key", "Unlocks locked locks or locks unlocked locks")]),(4,[]),(5,[("Yellow gem", "A small gem of yellow color in the shape of a hehagon. Possibly a topaz?")]),(6,[]),(7,[]),(8,[]),(9,[]),(10,[("Shimmering coat", "A coat that shimmers, casually"), ("White gem", "A beautiful white gem, must be worth a fortune")]),(11,[]),(12,[]),(13,[]),(14,[]),(15,[]),(16,[]),(17,[]),(18,[])]
+itemsList = [(1,[]), (2,[]),(3,[("Key", "Unlocks locked locks or locks unlocked locks")]),(4,[]),(5,[("Yellow gem", "A small gem of yellow color in the shape of a hexagon. Possibly a topaz?")]),(6,[]),(7,[]),(8,[]),(9,[]),(10,[("Shimmering coat", "A coat that shimmers, casually"), ("White gem", "A beautiful white gem, must be worth a fortune")]),(11,[]),(12,[]),(13,[]),(14,[]),(15,[]),(16,[]),(17,[]),(18,[])]
 
+objectList :: [(RoomNumber, [Object])]
+objectList = [(1,[("Door", "Stone" "And old wooden door without a handle.") ("A big stone wall", "", "At inspection a loose stone falls of the wall and you pick it up")]), (2,[]), (3,[("Painting", "", "Gloomy painting of an eldery man")]), (4,[]), (5,[ ("Chest", "Key", "A wooden chest, and its locked") ]), (6,[]), (7,[("Bridge", "", "The bridge is put in a vertical position, bu there seems to be a mechanism attached to it. Maybe there is a way to lower it for me to gain passage?"),("Piedestal", "Yellow Gem", "It has a small socket in it with six edges") ]), (8,[]), (9,[]), (10,[]), (11,[]), (12,[]), (13,[]), (14,[]), (15,[]), (16,[]), (17,[]), (18,[])] 
