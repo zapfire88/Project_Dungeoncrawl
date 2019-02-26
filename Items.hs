edbe0e51271c7df106b9e17d1a52cba0e7b5356d
zapfire88 <43233970+zapfire88@users.noreply.github.com>
2019-02-26 14:22:23 +0100

    Update Items.hs

--- Items.hs
+++ Items.hs
@@ -1,1 +1,9 @@
 
+
+-- Items --
+module Items(itemsList)
+where
+
+--itemList :: [(Int,[String])]
+itemsList = [(1,[]), (2,[]),(3,["key"]),(4,[]),(5,["Yellow gem"]),(6,[]),(7,[]),(8,[]),(9,[]),(10,["Shimmering coat", "White gem"]),(11,[]),(12,[]),(13,[]),(14,[]),(15,[]),(16,[]),(17,[]),(18,[])]
+
