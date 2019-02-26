b42183b19b00eb1a048a8c44ea1cea7835492a83
zapfire88 <43233970+zapfire88@users.noreply.github.com>
2019-02-26 15:26:08 +0100

    Update Adventure_crawl.hs

--- Adventure_crawl.hs
+++ Adventure_crawl.hs
@@ -1,40 +1,61 @@
 import Texts
+import Items
 -- Rooms --
 
 
-type Room = (RoomNumber, PossibleDirections, Description, PointOfInterest)
+type Room = (RoomNumber, PossibleDirections, PointOfInterests)
 type RoomNumber = Int
-type Description = (String, Bool)
-type Item = (String, String) -- Item is a Touple of two strings, first string for the items name and second string for the items description. 
-type PointOfInterest = [(Item, Item)]
+type Description = String
+type Item = String
+type PointOfInterests = [String]
 type PossibleDirections = [RoomNumber]
-type Door = [(Int, Bool)] -- Int is the index (of doors in room). First Bool is for whether it is open, second if it is locked.
-type Chest = [Bool]
-type AdjacentRooms = [(RoomNumber, RoomNumber)]
-
+type AdjacentRooms = [(Int, Int)]
 
 
 adjacentRoom :: RoomNumber -> PossibleDirections
 adjacentRoom position = [a | (a,b) <- adjacentRoomsList, position == b] ++ [b | (a,b) <- adjacentRoomsList, position == a]
 
-
-
-
 -- Edge list av alla rum --
 adjacentRoomsList :: AdjacentRooms
 adjacentRoomsList = [(1, 2),(2, 3),(2, 7),(3, 4),(4, 5),(4, 6),(6, 13),(7, 8),(8, 9),(8, 11),(9, 10),(11, 12),(13, 14),(13, 15),(15, 16),(16, 17),(17, 18)]
 
---start Room
-action <- getLine
-if action == 1 then putStrLn "Hey"
 
---runIntro = putStr ( unlines list) where list = ["Finally, after many years of searching, you are now standing in front of it.", "To think that there was something, either some sort of a force field, or even stranger, a magic field that could hide it.","I will definitely discover something amazing here!"] 
+actions :: [String]
+actions = ["- inspect", "- take", "- use", "- move", "- hit"]
 
-runGame ::
+
+start :: Room
+start = (position, possibleDirection, takeInterest 1) where
+  position = 1
+  possibleDirection = adjacentRoom position
+  --let description = textList!!position
+  --pointOfInterest = takeInterest position
+  --description
+  --putStr (unlines (takeInterest position))
+
+takeInterest position = [concat b | (a,b) <- itemList, position == a]
+
+
+runGame :: Room -> IO ()
+runGame state = do
+  textList!!1
+  putStrLn (unlines [" ", "What would you like to do?"])
+  putStrLn (unlines actions)
+  action <- getLine
+  if action == "inspect" then do putStrLn "1" 
+  else if action == "take" then do putStrLn "2"
+  else if action == "use" then do putStrLn "3"
+  else if action == "move" then do putStrLn "4"
+  else if action == "hit" then do putStrLn "5"
+  else if action == "quit" then do return ()
+  else do putStrLn ("wrong input: '" ++ action ++ "' is not valid.")
+  
 
 startGame :: IO ()
 startGame = do
-  runIntro
-  --runGame start
-
+  putStrLn (unlines [" ","To quit: write 'quit'"," "])
+  textList!!0
+  putStrLn " "
+  runGame start
+  return ()
 
