1885c5e62b42e647b00a09e462f30d4e4a4e46a9
jagtarenpaj <43341239+jagtarenpaj@users.noreply.github.com>
2019-02-26 14:19:55 +0100

    Update Actions.hs

--- Actions.hs
+++ Actions.hs
@@ -1,25 +1,48 @@
 
-module Actions(inspect, take, use, move, hit)
+module Actions(inspectItem, inspectObject, take, useItem, move)
 where
 -- Actions --
 
-inspect :: PointOfInterest -> IO String -> PointOfInterest -> String
+type Room = (RoomNumber, PossibleDirections, Description, [Object], [Item])
+type RoomNumber = Int
+type Description = (String, Bool)
+type Item = (String, String) -- Item is a Touple of two strings, first string for the items name and second string for the items description. 
+-- type PointOfInterest = undefined  
+type PossibleDirections = [RoomNumber]
+type Door = [(Int, Bool)] -- Int is the index (of doors in room). First Bool is for whether it is open, second if it is locked.
+type Object = (Item, String) -- Second string in touple is for description
+type AdjacentRooms = [(RoomNumber, RoomNumber)]
+type Inventory = [Item]
 
 
-take :: PointOfInterest -> IO String -> PointOfInterest -> Bag
-
-
-use :: Item -> PointOfInterest -> String
-
-
-move :: Room -> IO String -> Room
-
-
-hit :: PointOfInterest -> IO String -> PointOfInterest -> String
+adjacentRoom :: RoomNumber -> PossibleDirections
+adjacentRoom position = [a | (a,b) <- adjacentRoomsList, position == b] ++ [b | (a,b) <- adjacentRoomsList, position == a]
 
 
 
- 
+adjacentRoomsList :: AdjacentRooms
+adjacentRoomsList = [(1, 2),(2, 3),(2, 7),(3, 4),(4, 5),(4, 6),(6, 13),(7, 8),(8, 9),(8, 11),(9, 10),(11, 12),(13, 14),(13, 15),(15, 16),(16, 17),(17, 18)]
 
 
+dEIL ::(Eq a) => a -> [a] -> Bool
+dEIL x [] = False
+dEIL grunka (x:xs) = if grunka == x then True
+                     else dEIL grunka xs
+
+useitem :: Item -> Object -> String
+useitem sak (a,b) = if sak == a then "Utför event"
+                    else "Does not work"
+
+inspectItem :: Item -> String
+inspectItem (a,b) = show b
+
+inspectObject :: Object -> String
+inspectObject (a,b) = show b
+
+move :: RoomNumber -> RoomNumber -> String
+move dittrum villtill = if (dEIL (dittrum,villtill) adjacentRoomsList) == True then "runGame villtill"
+                        else "Cannot go there" 
+
+take :: Item -> Inventory
+take sak = sak : []
 
