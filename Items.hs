d259e26576d40c504ebc2df260577594c0a311d1
jagtarenpaj <43341239+jagtarenpaj@users.noreply.github.com>
2019-02-26 14:36:41 +0100

    Update Items.hs

--- Items.hs
+++ Items.hs
@@ -4,6 +4,6 @@
 module Items(itemsList)
 where
 
---itemList :: [(Int,[String])]
-itemsList = [(1,[]), (2,[]),(3,["key"]),(4,[]),(5,["Yellow gem"]),(6,[]),(7,[]),(8,[]),(9,[]),(10,["Shimmering coat", "White gem"]),(11,[]),(12,[]),(13,[]),(14,[]),(15,[]),(16,[]),(17,[]),(18,[])]
+--itemList :: [(RoomNumber,[Item])]
+itemsList = [(1,[]), (2,[]),(3,[("key", "Unlocks locked locks or locks unlocked locks")]),(4,[]),(5,[("Yellow gem", "A small gem of yellow color. Possibly a topaz?")]),(6,[]),(7,[]),(8,[]),(9,[]),(10,[("Shimmering coat", "A coat that shimmers, casually"), ("White gem", "A beautiful white gem, must be worth a fortune")]),(11,[]),(12,[]),(13,[]),(14,[]),(15,[]),(16,[]),(17,[]),(18,[])]
 
