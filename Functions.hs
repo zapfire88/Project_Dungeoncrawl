462e097183f95342223dd233b48e587e42a03243
zapfire88 <43233970+zapfire88@users.noreply.github.com>
2019-02-28 20:03:41 +0100

    Update Functions.hs

--- Functions.hs
+++ Functions.hs
@@ -1,11 +1,15 @@
 
-module Functions(takeThird, removeItem, roomItem, itemDesc, roomObject, objectDesc, adjacentRoom, adjacentRoomsList) 
+module Functions(takeFirst, takeSecond, takeThird, removeItem, removeContent, roomItem, itemDesc, roomObject, objectDesc, eventDesc, adjacentRoom, adjacentRoomsList, changeBool, getMoves, getMovesText) 
 where
 
 import GameContents
 
-takeThird loc = [c | (a,b,c) <- adjacentRoomsList, loc == b] ++ [c | (a,b,c) <- adjacentRoomsList, loc == a]
+
+takeFirst (a,b,c) = a
+takeSecond (a,b,c) = b
+takeThird (a,b,c) = c
   
+--testfunc
 
 --removeItem :: Item -> [Item] -> [Item]
 removeItem _ [] = []
@@ -13,6 +17,12 @@ removeItem item (x:xs)
   | item == x = removeItem item xs
   | otherwise = x:removeItem item xs
 
+
+
+--removeContent _ [] = []
+removeContent input content = filter (\content -> (snd content) /= input)
+ -- item == x = removeItem item xs
+ -- | otherwise = x:removeItem item xs
 
 --roomItem :: RoomNumber -> [Item]
 roomItem loc = [concat b | (a,b) <- itemList, loc == a]
@@ -27,8 +37,17 @@ roomObject loc = [concat b | (a,b) <- objectList, loc == a]
 --objectDesc :: RoomNumber -> Object -> Description
 objectDesc loc object = [concat (tail b) | (a,b) <- objectDescriptionList, loc == a && object == (head b)]
 
+
+eventDesc loc action = [concat ([last b]) | (a,b) <- eventDescriptionList, loc == a && action == (head b)]
 --adjacentRoom :: RoomNumber -> PossibleDirections
-adjacentRoom loc = [a | (a,b,c) <- adjacentRoomsList, loc == b] ++ [b | (a,b,c) <- adjacentRoomsList, loc == a]
+--adjacentRoom loc = [a | (a,b,c) <- adjacentRoomsList, loc == b] ++ [b | (a,b,c) <- adjacentRoomsList, loc == a]
 
+--eventChange loc action = 
 
-  
+getMovesText loc = [concat c | (a,b,c) <- moveList, loc == a]
+getMoves loc = [b | (a,b,c) <- moveList, loc == a]
+
+adjacentRoom loc = [(a,c) | (a,b,c) <- adjacentRoomsList, loc == b] ++ [(b,c) | (a,b,c) <- adjacentRoomsList, loc == a]
+
+changeBool loc = [(a,True) | (a,b) <- (adjacentRoom loc), b == False] ++ [(a,True) | (a,b) <- (adjacentRoom loc), b == True]
+
