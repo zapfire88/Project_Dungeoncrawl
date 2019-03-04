a993d6ca7dd213de96966612c2fa22c93d07b719
zapfire88 <43233970+zapfire88@users.noreply.github.com>
2019-03-04 03:00:23 +0100

    Update GameContents.hs

--- GameContents.hs
+++ GameContents.hs
@@ -1,30 +1,32 @@
 
 
 -- Game contents --
-module GameContents(takeSecond, changeMoveState, flatten, removeItem, removeContent, checkRoom, roomItem, itemDesc, roomObject, objectDesc, eventDesc, getMovesText, getMoves, changeBool, checkEvent, newTextList, contentList, descriptionList, textList, eventDescriptionList, moveList)
+module GameContents(checkMove, changeMoveState, flatten, removeItem, removeContent, roomItem, itemDesc, roomObject, objectDesc, eventDesc, getMovesText, getMoves, getNewRoom, checkEvent, newTextList, contentList, descriptionList, eventDescriptionList, moveList)
 where
 
-import Texts
+--import Texts
 
 ---- Functions ----
+--getNewRoom loc input moveStates = takeSecond (checkRoom loc input moveStates)
 
-takeSecond [(a,b,c,d)] = b
 
 changeMoveState loc moveState = flatten(changeBool loc moveState):filteredList where
   filteredList = filterList (reverseChangeBool(flatten(changeBool loc moveState))) moveState
   filterList state moveState = filter (/=state) moveState
   reverseChangeBool (a,b,c,d) = (a,b,c, not d)
+  changeBool loc list = [(a,b,c,True) | (a,b,c,d) <- list, a == loc && d == False]
 
 flatten [a] = a
 
 removeItem input items = filter (/=input) items
+
 removeContent input content = filter (\(_,b,c,_) -> b == 2 && c == input) content
 
-checkRoom loc input list = filter (\(a,_,c,_) -> a == loc && c == input) list
+
 
 roomItem loc = [c | (a, b, c, d) <- contentList, loc == a && b == 1 && d == True]
 
-roomObject loc = [c | (a, b, c, d) <- contentList, loc == a && b == 2 && d == True]
+roomObject loc = "Self":[c | (a, b, c, d) <- contentList, loc == a && b == 2 && d == True]
 
 itemDesc loc item = [last c | (a,b,c) <- descriptionList, loc == a && b == 1 && item == (head c)]
 
@@ -37,7 +39,11 @@ getMovesText loc list = [c | (a,b,c,d) <- list, loc == a && d == True]
 
 getMoves loc list = [(a,b,c,d) | (a,b,c,d) <- list, loc == a && d == True]
 
-changeBool loc list = [(a,b,c,True) | (a,b,c,d) <- list, a == loc && d == False]
+checkMove loc input list = flatten [(a,b,c,d) | (a,b,c,d) <- list, loc == a && input == c && d == True]
+
+getNewRoom loc input moveStates = takeSecond (checkRoom loc input moveStates) where
+  takeSecond (a,b,c,d) = b
+  checkRoom loc input list = flatten (filter (\(a,_,c,_) -> a == loc && c == input) list)
 
 newTextList loc list = (take loc list) ++ changeTextBool [(list!!(loc))] ++ (drop (loc+1) list) where
   changeTextBool list = [(True,b) | (a,b) <- list, a == False] ++ [(True,b) | (a,b) <- list, a == True]
@@ -47,12 +53,12 @@ checkEvent loc action actionOn = if elem action (getEvent loc action) == True &&
   
   
 ---- Item- and Object-lists ----
+ 
+contentList = [(1, 2, "Door", True), (1, 2, "Stone wall", True), (2, 1, "Torch", True), (3, 2, "Painting", True), (4, 2, "Fireplace",True), (5, 1, "Yellow gem", True), (7, 2, "Bridge", True), (7, 2, "Pedestal", True), (9, 1, "Shimmering coat", True), (9, 1, "White gem", True), (10, 2, "Sphinx", True), (12, 1, "Scroll", True)]
 
-contentList = [(1, 2, "Door", True), (1, 2, "Stone wall", True), (2, 1, "Torch", True), (3, 2, "Painting", True), (4, 2, "Fireplace",True), (5, 1, "Yellow gem", True), (5, 2, "Chest", True), (7, 2, "Bridge", True), (7, 2, "Pedestal", True), (10, 1, "Shimmering coat", True), (10, 1, "White gem", True)]
+descriptionList = [(1, 2, ["Door", "An old wooden door without a handle."]), (1, 2, ["Stone wall", "There seem to be a 'loose' stone on the wall"]), (2, 1, ["Torch", "It gives of a weird light"]), (3, 2, ["Painting", "It is a gloomy painting of an eldery man."]), (3, 2, ["Door", "It is a wooden door with a keyhole"]), (5, 1, ["Yellow gem", "A small gem of yellow color in the shape of a hexagon."]), (7, 2, ["Bridge", "The bridge is raised. You need to lower it somehow."]), (7, 2, ["Pedestal", "There is a small socket in it. If the right thing is socketed in it, the bridge might be lowered."]), (9, 1, ["Shimmering coat", "It shimmers in changing colours. Maybe it has some magic properties?"]), (9, 1, ["White gem", "A beautiful white gem. It might be worth a fortune."]), (11, 1, ["Scroll", "It is a rolled up parchment. Something is written but you can't seem to understand it."])]
 
-descriptionList = [(1, 2, ["Door", "An old wooden door without a handle."]), (1, 2, ["Stone wall", "There seem to be a 'loose' stone on the wall"]), (2, 1, ["Torch", "It gives of a weird light"]), (3, 1, ["Key","It is a brass key."]), (3, 2, ["Painting", "Gloomy painting of an eldery man."]), (3, 2, ["Door", "It is a wooden door with a keyhole"]), (5, 1, ["Yellow gem", "A small gem of yellow color in the shape of a hexagon."]), {-(5, 2, ["Chest", "A wooden chest. It is locked"]),-} (7, 2, ["Bridge", "The bridge is put in a vertical position, but there seems to be a mechanism attached to it. Maybe there is a way to lower it for me to gain passage?"]), (7, 2, ["Pedestal", "It has a small socket in it with six edges"]), (10, 1, ["Shimmering coat", "A coat that shimmers in a magic"]), (10, 1, ["White gem", "A beautiful white gem, must be worth a fortune"])]
+eventDescriptionList = [(1, 2, ["Stone wall", "", "You push the stone and the door opens."]), (3, 1, ["Door", "Key", "You unlock the door."]), (3, 2, ["Painting", "", "While looking closer at the painting you notice that one of the eyes on the man seems to be a button. When you press it the door opens."]), (4, 1, ["Fireplace", "Torch", "A secret door opens as soon as you light up the fireplace"]), (7, 1, ["Pedestal", "Yellow gem", "The bridge is lowered! Maybe the gem holds some sort energy?"]), (10, 2, ["Self", "Shimmering coat", "You test the coat against the shimmering wall. It passes through. You put it on and continue to the next room."])]
 
-eventDescriptionList = [(1, 2, ["Stone wall", "", "You push the stone and the door opens."]), (3, 1, ["Door", "Key", "You unlock the door."]), (3, 2, ["Painting", "", "While looking closer at the painting you notice that one of the eyes on the man seems to be a button. When you press it the door opens."]), (4, 1, ["Fireplace", "Torch", "A secret door opens as soon as you light up the fireplace"]), (7, 1, ["Pedestal", "Yellow gem", "The bridge is lowered! Maybe the gem holds some sort energy?"])]
-
-moveList = [(1,2,"Inside",False), (2,1,"Back",False), (2,3,"Right",True), (2,7,"Forward",True), (3,2,"Back",True), (3,4,"Forward",False), (4,5,"Through hidden door",False), (4,6,"Forward",True), (7,13,"Up",False), (7,2,"Back",True)]
+moveList = [(1,2,"Inside",False), (2,3,"Right",True), (2,7,"Forward",True), (3,2,"Back",True), (3,4,"Forward",False), (4,5,"Through hidden door",False), (4,6,"Forward",True), (5,4,"Back",True), (6,5,"Back",True), (6,5,"Back",True), (7,2,"Back",True), (7,8,"Over the bridge",False), (8,7,"Back",True), (8,9,"Right",True), (8,10,"Left",True), (9,8,"Back",True), (10,8,"Back",True), (10,11,"Forward",False), (11,10,"Back",True)]
 
